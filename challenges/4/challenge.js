e86a2a747b6fcab9df623feaf472008cbb622943
everton <everton@Evertons-MacBook-Air.local>
2020-10-19 17:47:44 -0300

    Challenge 4

--- challenges/4/challenge.js
+++ challenges/4/challenge.js
@@ -52,6 +52,57 @@
  *  }
  */
 
-const normalizeData = unormalized => {}
+
+const unormalized = {
+    "id": "6197b77e-3942-11ea-a137-2e728ce88125",
+    "user": {
+        "id": "6197ba94",
+        "name": "Laura"
+    },
+    "reports": [{
+        "id": "51ddf1a9",
+        "result": {
+            "document": "356.4325-10",
+            "status": "em análise",
+        }
+    }]
+}
+
+
+const normalizeData = unormalized => {
+    const {
+        id,
+        user,
+        reports
+    } = unormalized
+
+    const normalized = `
+            {
+                "results": {
+                    "${id}": {
+                        "id": "${id}", 
+                        "user": "${user.name}", 
+                        "reports": "[${reports[0].id}]"
+                    }
+                },
+                "users": {
+                    ${user.id}: {
+                        "${user.id}": "${user.id}", "name": "${user.name}"
+                    }
+                },
+                "reports": {
+                    "${reports[0].id}": {
+                        "id": "${reports[0].id}", 
+                        "user": "${user.id}", 
+                        "document", "${reports[0].result.document}", 
+                        "status", "${reports[0].result.status}"
+                }
+            }
+        `
+
+    return normalized
+}
+
+console.log(normalizeData(unormalized))
 
 module.exports = normalizeData
