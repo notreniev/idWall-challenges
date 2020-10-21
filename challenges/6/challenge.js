6de7dfd83944e3178112c63a7ff8e2b3bf5dd8bc
everton <everton@Evertons-MacBook-Air.local>
2020-10-20 21:14:08 -0300

    Chellenge 6 almost passed

--- challenges/6/challenge.js
+++ challenges/6/challenge.js
@@ -26,6 +26,64 @@
  * }
  */
 
-const extractSize = htmlTemplate => {}
+// const htmlTemplate = `<div style="background-color: red;"> <img style="width: 120px; height: 20%" /></div>`
+const htmlTemplate = `<div style="height: 20px; width: 60px;">`
+// const htmlTemplate = `
+// <div style="width: 442px;">
+//   <span style="height: 911px;"></span>
+//   <span style="height: 121px;"></span>
+// </div>
+// `
+
+const convertArrayToObject = (array, key) => {
+    const initialValue = {};
+    return array.reduce((obj, item) => {
+        return {
+            ...obj,
+            [item[key]]: item,
+        };
+    }, initialValue);
+};
+
+const extractSize = htmlTemplate => {
+
+    if (!htmlTemplate) return {
+        width: 0,
+        height: 0
+    }
+
+    const matches = htmlTemplate.match(/\bstyle=(['"])(.*?)\1/gi);
+    var styles = [];
+    for (var i = 0; i < matches.length; i++) {
+        styles.push(matches[i].substring(7, matches[i].length - 1));
+    }
+
+    // console.log(styles)
+
+    const results = styles.filter((s, i) => {
+        if (styles[i].startsWith('width') || styles[i].startsWith('height')) {
+            //console.log(styles[i])
+            return styles
+        }
+    })
+
+    //console.log(results)
+
+
+    let atributos = results[0].split(";")
+    let prop1 = `"${atributos[0].split(":")[0].trim()}"`
+    let val1 = `${parseInt(atributos[0].split(":")[1])}`
+
+    let prop2 = `"${atributos[1].split(":")[0].trim()}"`
+    let val2 = `${parseInt(atributos[1].split(":")[1])}`
+
+    const retorno = JSON.parse(`{${prop1}: ${val1},${prop2}: ${val2}}`)
+
+    console.log(retorno)
+
+    return retorno
+}
+
+extractSize(htmlTemplate)
 
 module.exports = extractSize
--- challenges/6/challenge.test.js
+++ challenges/6/challenge.test.js
@@ -5,14 +5,20 @@ describe('Challenge 6', () => {
   [INPUt]: '<div style="height: 20px; width: 60px;"></div>'`, async () => {
     const htmlTemplate = `<div style="height: 20px; width: 60px;"></div>`
 
-    expect(extractSize(htmlTemplate)).toEqual({ height: 20, width: 60 })
+    expect(extractSize(htmlTemplate)).toEqual({
+      height: 20,
+      width: 60
+    })
   })
 
   test(`It should return {width: 120, height: 20}
   [INPUT]: '<div style="background-color: red;"> <img style="width: 120px; height: 20%" /></div>'`, async () => {
     const htmlTemplate = `<div style="background-color: red;"> <img style="width: 120px; height: 20%" /></div>`
 
-    expect(extractSize(htmlTemplate)).toEqual({ width: 120, height: 20 })
+    expect(extractSize(htmlTemplate)).toEqual({
+      width: 120,
+      height: 20
+    })
   })
 
   test(`It should return {width: 442, height: 911}
@@ -29,13 +35,19 @@ describe('Challenge 6', () => {
     </div>
     `
 
-    expect(extractSize(htmlTemplate)).toEqual({ width: 442, height: 911 })
+    expect(extractSize(htmlTemplate)).toEqual({
+      width: 442,
+      height: 911
+    })
   })
 
   test(`It should return {width: 0, height: 0}
   [INPUT]: ''`, async () => {
     const htmlTemplate = ``
 
-    expect(extractSize(htmlTemplate)).toEqual({ width: 0, height: 0 })
+    expect(extractSize(htmlTemplate)).toEqual({
+      width: 0,
+      height: 0
+    })
   })
 })
